517a8eeed2a6a9f370307e0f713ebdd7ea64e142
Dan Groleau <ddgroleau@gmail.com>
2022-02-05 16:04:32 -0700

    just testing some flexbox properties

--- index.php
+++ index.php
@@ -1,58 +1,12 @@
-<?php include("./views/partials/header.php"); ?>
-
-        <div class="home-box">
-            <div class="home-content">
-                <h1>Dan Groleau</h1>
-                <h3>Software Engineer</h3>
-                <nav class="nav">
-                    <a class="home-link btn" href="/projects">Projects</a>
-                    <a class="home-link btn" href="/tutorials">Tutorials</a>
-                    <a class="home-link btn" href="#about-anchor">About</a>
-                </nav>
-            </div>
+<html>
+    <head>
+        <link rel="stylesheet" href="/assets/css/app.css" />    
+    </head>
+    <body>
+        <div class="parent">
+            <div class="container flex">1</div>
+            <div class="container flex">2</div>
+            <div class="container flex">3</div>
         </div>
-        <div id="about-anchor" class="about-container">
-            <div class="skills-box">
-                    <img class="img-scale-md img-hover" href="https://dotnet.microsoft.com/en-us/" src="/assets/media/csharp.svg">
-                    <img class="img-scale-md img-hover" href="https://reactjs.org/" src="/assets/media/reactjs.svg">
-                    <img class="img-scale-lg img-hover" href="https://nodejs.org/en/" src="/assets/media/nodejs.svg">
-                    <img class="img-scale-md img-hover" href="https://www.typescriptlang.org/" src="/assets/media/typescript.svg">
-                    <img class="img-scale-md img-hover" href="https://sass-lang.com/" src="/assets/media/sass.svg">
-                    <img class="img-scale-lg img-hover" href="https://www.php.net/" src="/assets/media/php.svg">
-            </div>
-            <div class="about-box">
-                <p>
-                    I am a software engineer specializing in Web Development. I enjoy working in C#, JavaScript, PHP, HTML, CSS and SQL. 
-                I use NodeJs, React, NestJs, NextJs, Typescript, Blazor, ASP.NET and Sass/SCSS.
-                </p>
-                <p>
-                    I have experience working with MongoDb, 
-                SQL Server, MySQL, and PostgreSQL. I regularly use Docker, Octopus Deploy, and Azure and Team City products. I am
-                an advocate for test driven development, object-oriented programming and clean code principles.
-                </p>
-                 <a class="link return-home" href="/">Return to Home</a>
-            </div>
-            <?php include("./views/partials/footer.php"); ?>
-        </div>
-
-<script type="text/javascript">
-    document.getElementById("nav-header").style ="display:none";
-    document.getElementById("home").style="display:none";
-    document.getElementById("about").style="display:none";
-    
-    let navbarDropdown = document.getElementById("navbar-dropdown")
-    navbarDropdown.className ="navbar-dropdown-home";
-
-    window.onscroll = () => { 
-    "use strict";
-    if (document.body.scrollTop >= 600 ) {
-        document.getElementById("navbar-dropdown").className ="navbar-dropdown";
-    } 
-    else {
-        document.getElementById("navbar-dropdown").className ="navbar-dropdown-home";
-    }
-};
-</script>
-
-
-
+    </body>
+</html>
--- views/partials/footer.php
+++
@@ -1,8 +0,0 @@
-            <footer>
-                <div class="footer-box">
-                    <strong><?php echo date("Y"); ?> | Dan Groleau</strong>
-                    <a href="contact">Contact</a>
-                </div>
-            </footer>
-    </body>
-</html>
